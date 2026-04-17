1ddbe99811cd3338a58f1df1ac04b1f1c709c96f
maddyolivi <madison.oneill@student.northampton.edu>
2026-04-17 12:49:14 -0400

    testing player functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 ﻿#include <iostream>
 
+#include "Characters/Player.h"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Player player1;
+
+    cout << "Health: " << player1.getHealth() << endl;
+    cout << "Take 10 damage." << endl;
+
+    player1.takeDamage(10);
+
+    cout << "Health: " << player1.getHealth() << endl;
+
     return 0;
 }
