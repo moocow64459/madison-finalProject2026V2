b32a702d85bdfc63b65bece4febb4dcf08824bd7
maddyolivi <madison.oneill@student.northampton.edu>
2026-04-17 12:49:10 -0400

    develop player class
    base functions like health getters/setters
    take damage

---
+++ Characters/Player.h
@@ -0,0 +1,25 @@
+﻿#ifndef PLAYER_H
+#define PLAYER_H
+#include <string>
+
+using namespace std;
+
+class Player {
+private:
+    string name;
+    int currentHealth;
+    int maxHealth;
+
+    void setHealth(int);
+public:
+    Player();
+    Player(const string&, int, int);
+
+    void attack();
+    int takeDamage(int);
+
+    int getHealth() const;
+};
+
+
+#endif //PLAYER_H
