d34f1102baa736c3e1e420dc93ae02dd87249de0
maddyolivi <madison.oneill@student.northampton.edu>
2026-04-17 13:38:03 -0400

    testing attacks

--- main.cpp
+++ main.cpp
@@ -14,6 +14,21 @@ int main() {
 
     cout << "Player 2 Attacking Player 1"<< endl;
     player2.attack(player1);
+    player2.attack(player1);
+    player2.attack(player1);
+    player2.attack(player1);
+    player2.attack(player1);
+    player2.attack(player1);
+    player2.attack(player1);
+    player2.attack(player1);
+    player2.attack(player1);
+    player2.attack(player1);
+    player2.attack(player1);
+
+
+    cout << "Player 1 Health: " << player1.getHealth() << endl;
+    cout << "Player 2 Health: " << player2.getHealth() << endl;
+    
 
     return 0;
 }
