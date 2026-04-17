f6d2686f49323d7096629142b76bf72eaeb6e136
maddyolivi <madison.oneill@student.northampton.edu>
2026-04-17 12:55:10 -0400

    Player works with both regular and parameterized constructor

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include "Characters/Player.h"
 
 int main() {
-    Player player1;
+    Player player1("Madison", 100, 100);
 
     cout << "Health: " << player1.getHealth() << endl;
     cout << "Take 10 damage." << endl;
