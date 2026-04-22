3d4cfcab71ae5e4664d8e5004af7e7c94c7c8ea4
maddyolivi <madison.oneill@student.northampton.edu>
2026-04-22 13:35:18 -0400

    fists weapon override

---
+++ Weapons/Fists.cpp
@@ -0,0 +1,11 @@
+﻿#include "Fists.h"
+
+void Fists::attack(Character &target) {
+    // this is where the weapon will deal damage
+}
+
+void Fists::defend() {
+    // this is where a defensive call is stored
+    // if defense > attack damage, no attack damage
+    // if defense < attack damage, take damage
+}
---
+++ Weapons/Fists.h
@@ -0,0 +1,13 @@
+﻿#ifndef FISTS_H
+#define FISTS_H
+#include "Weapon.h"
+
+
+class Fists : public Weapon {
+public:
+    void attack(Character &) override;
+    void defend() override;
+};
+
+
+#endif //FISTS_H
