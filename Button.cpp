41cddd14fd2e8dd08fc3959c3e4d50965af94e7c
maddyolivi <madison.oneill@student.northampton.edu>
2026-04-23 13:59:32 -0400

    create button

---
+++ Button.cpp
@@ -0,0 +1,5 @@
+﻿//
+// Created by Madison_PC on 4/23/2026.
+//
+
+#include "Button.h"
---
+++ Button.h
@@ -0,0 +1,15 @@
+﻿#ifndef BUTTON_H
+#define BUTTON_H
+
+
+class Button {
+protected:
+
+public:
+    class PixelText{
+
+    };
+};
+
+
+#endif //BUTTON_H
