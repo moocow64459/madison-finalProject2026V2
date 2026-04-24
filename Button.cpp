52a301a1a85f8a46bf9c23ccf165897a6c373923
maddyolivi <madison.oneill@student.northampton.edu>
2026-04-24 15:23:42 -0400

    Button class def
    TODO- add == operator that accounts for the size of the button from the origin

---
+++ Button.cpp
@@ -0,0 +1,26 @@
+﻿#include "Button.h"
+
+void Button::draw(sf::RenderTarget &target, sf::RenderStates states) const {
+    target.draw(shape, states);
+}
+
+Button::Button() : shape(sf::Vector2f(512, 108)) {
+    shape.setOrigin({256, 54});
+    shape.setPosition({960, 544});
+    shape.setFillColor(sf::Color::White);
+}
+
+void Button::setButtonText(const std::string&) {
+    //sf::Text text()
+}
+
+void Button::buttonClicked(const sf::Vector2f mousePosition, const sf::RectangleShape &button) {
+    sf::Vector2f buttonPosition = button.getPosition();
+    bool isClicked = mousePosition == buttonPosition;
+}
+
+sf::Vector2f Button::getMouseVector2f(const sf::Window &window) const {
+    float posX = static_cast<float>(sf::Mouse::getPosition(window).x);
+    float posY = static_cast<float>(sf::Mouse::getPosition(window).y);
+    return sf::Vector2f(posX, posY);
+}
---
+++ Button.h
@@ -0,0 +1,21 @@
+﻿#ifndef BUTTON_H
+#define BUTTON_H
+#include <SFML/Graphics.hpp>
+
+
+class Button : public sf::Drawable{
+private:
+    sf::RectangleShape shape;
+
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+public:
+    Button();
+    static void setButtonText(const std::string&);
+
+    bool buttonClicked(sf::Vector2f, const sf::RectangleShape &);
+
+    sf::Vector2f getMouseVector2f(const sf::Window&) const;
+};
+
+
+#endif //BUTTON_H
