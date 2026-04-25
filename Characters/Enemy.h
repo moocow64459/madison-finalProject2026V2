c13e573832360e8ba23181aafe4d31216d238455
maddyolivi <madison.oneill@student.northampton.edu>
2026-04-25 14:37:59 -0400

    enemy base class
    will have a spawn probability that changes based on location/level/type of enemy/etc. in the future

--- Characters/Enemy.h
+++ Characters/Enemy.h
@@ -1,8 +1,18 @@
 ﻿#ifndef ENEMY_H
 #define ENEMY_H
+#include "../Character.h"
 
 
-class Enemy {
+class Enemy : public Character {
+private:
+    int spawnProbability;
+public:
+    using Character::Character;
+    Enemy();
+    Enemy(int);
+
+    void setSpawnProbability(int);
+    int getSpawnProbability() const;
 };
 
 
