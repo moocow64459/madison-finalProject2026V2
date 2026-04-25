584991c35950d9a07e6148f4b8138f6aa09a6801
maddyolivi <madison.oneill@student.northampton.edu>
2026-04-25 15:02:40 -0400

    begin character spawner

--- Characters/CharacterSpawner.cpp
+++ Characters/CharacterSpawner.cpp
@@ -1,1 +1,13 @@
 ﻿#include "CharacterSpawner.h"
+
+CharacterSpawner::CharacterSpawner() {
+    enemy = nullptr;
+}
+
+CharacterSpawner::CharacterSpawner(Enemy *enemyType) {
+    enemy = enemyType;
+}
+
+Enemy* CharacterSpawner::spawnEnemy() {
+    return new Enemy();
+}
--- Characters/CharacterSpawner.h
+++ Characters/CharacterSpawner.h
@@ -1,8 +1,16 @@
 ﻿#ifndef CHARACTERSPAWNER_H
 #define CHARACTERSPAWNER_H
+#include "Enemy.h"
 
 
 class CharacterSpawner {
+private:
+    Enemy* enemy;
+public:
+    CharacterSpawner();
+    CharacterSpawner(Enemy*);
+
+    Enemy* spawnEnemy();
 };
 
 
