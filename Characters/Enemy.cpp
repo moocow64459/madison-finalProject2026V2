d83f3e6125f01e5c55201292e64c34dbc1524d31
maddyolivi <madison.oneill@student.northampton.edu>
2026-04-29 16:36:05 -0400

    enemy class

---
+++ Characters/Enemy.cpp
@@ -0,0 +1,5 @@
+﻿//
+// Created by Madison_PC on 4/29/2026.
+//
+
+#include "Enemy.h"
---
+++ Characters/Enemy.h
@@ -0,0 +1,12 @@
+﻿#ifndef ENEMY_H
+#define ENEMY_H
+
+#include "../Character.h"
+
+class Enemy : public Character {
+    using Character::Character;
+    Enemy();
+};
+
+
+#endif //ENEMY_H
