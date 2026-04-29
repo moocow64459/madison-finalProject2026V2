419f15b8fea577c54ad772f0ac888c8031139424
maddyolivi <madison.oneill@student.northampton.edu>
2026-04-29 16:58:11 -0400

    testing in main

--- main.cpp
+++ main.cpp
@@ -1,27 +1,32 @@
 ﻿#include <iostream>
 #include "Characters/Player.h"
 #include "Weapon.h"
+#include "Characters/Enemy.h"
 #include "Weapons/Fists.h"
+#include "Weapons/ShortSword.h"
 
 int main() {
 
     Player player1("Madison", 300, 300, new Fists());
-    Player player2("Gene", 300, 300, new Fists());
+    Enemy enemy1("Gene", 300, 300, new ShortSword());
 
     cout << "Player 1 Health: " << player1.getHealth() << endl;
-    cout << "Player 2 Health: " << player2.getHealth() << endl;
+    cout << "Enemy Health: " << enemy1.getHealth() << endl;
 
     cout << endl;
-    player1.attack(player2);
-    cout << "Player 2 Health: " << player2.getHealth() << endl;
-    player1.attack(player2);
-    cout << "Player 2 Health: " << player2.getHealth() << endl;
-    player1.attack(player2);
-    cout << "Player 2 Health: " << player2.getHealth() << endl;
+    player1.attack(enemy1);
+    cout << "Enemy Health: " << enemy1.getHealth() << endl;
+
+    enemy1.attack(player1);
+    cout << "Player 1 Health: " << player1.getHealth() << endl;
+    cout << "Enemy Health: " << enemy1.getHealth() << endl;
+
+    player1.attack(enemy1);
+    cout << "Enemy Health: " << enemy1.getHealth() << endl;
     cout << endl;
 
     cout << "Player 1 Health: " << player1.getHealth() << endl;
-    cout << "Player 2 Health: " << player2.getHealth() << endl;
+    cout << "Enemy Health: " << enemy1.getHealth() << endl;
 
     return 0;
 }
